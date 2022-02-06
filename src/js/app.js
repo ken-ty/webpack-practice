cb6d67463445ab241abf598266cadffc12daf9c7
todoggler <kentokuraCS@gmail.com>
2022-02-06 22:52:47 +0900

    feat: app.jsで結果を表示
    h1タグのfadeinも追加。

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,8 @@
 import $ from 'jquery';
 import velocity from 'velocity-animate';
 import { add } from './modules/math';
+
+const result = add(1, 2);
+
+screen('body').append(result);
+velocity($('h1 '), 'fadein', { duration: 2000, loop: true});
